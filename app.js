49954459055b310ef3806bae32565c3f55c64834
Bob Taylor <btaylor@elliance.com>
2026-06-30 11:00:10 -0400

    Moved > inside span

--- app.js
+++ app.js
@@ -357,7 +357,7 @@ function renderPrograms(data, isSearching) {
                 <div class="program-cards__item__text">
                     <p class="program-cards__item__degree">${p.levelsOfStudy[0] || ''}</p>
                     <h3 class="program-cards__item__title">
-                        ${p.title} <span class="program-cards__item__title__indicator"></span>›
+                        ${p.title} <span class="program-cards__item__title__indicator">›</span>
                     </h3>
                     <div class="program-cards__item__attributes">
                         <p class="program-cards__item__course__numbers">${(p.credentials || []).join(', ')}</p>
